5638d3fe671470f20bfcd52d08e560242d4e684d
santiorozco18 <orozabarc029@gmail.com>
2026-08-13 17:44:38 -0600

    Update EstadoPedidoRepository.java

--- repuestos-cr/src/main/java/com/autopartescr/repuestos/repository/EstadoPedidoRepository.java
+++ repuestos-cr/src/main/java/com/autopartescr/repuestos/repository/EstadoPedidoRepository.java
@@ -1,7 +1,10 @@
 package com.autopartescr.repuestos.repository;
 
 import com.autopartescr.repuestos.domain.EstadoPedido;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface EstadoPedidoRepository extends JpaRepository<EstadoPedido, Integer> {
+
+    Optional<EstadoPedido> findByNombre(String nombre);
 }
